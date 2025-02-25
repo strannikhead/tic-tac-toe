817706c96d3d8ab664b671d8687f0845ed1ad7e2
strannikhead <144272555+strannikhead@users.noreply.github.com>
2025-02-25 17:21:19 +0500

    add tryFindWinner for any size fields

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function cellClickHandler(row, col) {
                 let coordinates = winner[1][i];
                 renderSymbolInCell(winner[0], coordinates[0], coordinates[1], '#FF0000');
             }
-            
+
             alert(`Winner is ${playersDictionary[winner[0]]}`);
         }
     }
@@ -98,29 +98,117 @@ function resetClickHandler() {
     renderGrid(3);
 }
 
-function tryFindWinner() {
-    const winCombinations = [
-        // Ряды
-        [[0, 0], [0, 1], [0, 2]],
-        [[1, 0], [1, 1], [1, 2]],
-        [[2, 0], [2, 1], [2, 2]],
-        // Столбцы
-        [[0, 0], [1, 0], [2, 0]],
-        [[0, 1], [1, 1], [2, 1]],
-        [[0, 2], [1, 2], [2, 2]],
-        // Диагонали
-        [[0, 0], [1, 1], [2, 2]],
-        [[0, 2], [1, 1], [2, 0]]
-    ];
+// function tryFindWinner() {
+//     const winCombinations = [
+//         // Ряды
+//         [[0, 0], [0, 1], [0, 2]],
+//         [[1, 0], [1, 1], [1, 2]],
+//         [[2, 0], [2, 1], [2, 2]],
+//         // Столбцы
+//         [[0, 0], [1, 0], [2, 0]],
+//         [[0, 1], [1, 1], [2, 1]],
+//         [[0, 2], [1, 2], [2, 2]],
+//         // Диагонали
+//         [[0, 0], [1, 1], [2, 2]],
+//         [[0, 2], [1, 1], [2, 0]]
+//     ];
+//
+//     for (let combination of winCombinations) {
+//         const [[x1, y1], [x2, y2], [x3, y3]] = combination;
+//         const cell = field[x1][y1];
+//         if (cell !== EMPTY && cell === field[x2][y2] && cell === field[x3][y3]) {
+//             isOver = true;
+//             return [cell, combination];
+//         }
+//     }
+//     return false;
+// }
 
-    for (let combination of winCombinations) {
-        const [[x1, y1], [x2, y2], [x3, y3]] = combination;
-        const cell = field[x1][y1];
-        if (cell !== EMPTY && cell === field[x2][y2] && cell === field[x3][y3]) {
+function tryFindWinner() {
+    const n = field.length;
+    if (n === 0) return false;
+
+    // Проверка строк
+    for (let i = 0; i < n; i++) {
+        const first = field[i][0];
+        if (first === EMPTY) continue;
+        let win = true;
+        for (let j = 1; j < field[i].length; j++) {
+            if (field[i][j] !== first) {
+                win = false;
+                break;
+            }
+        }
+        if (win) {
+            const combination = [];
+            for (let j = 0; j < field[i].length; j++) {
+                combination.push([i, j]);
+            }
             isOver = true;
-            return [cell, combination];
+            return [first, combination];
         }
     }
+
+    // Проверка столбцов
+    for (let j = 0; j < n; j++) {
+        const first = field[0][j];
+        if (first === EMPTY) continue;
+        let win = true;
+        for (let i = 1; i < n; i++) {
+            if (field[i][j] !== first) {
+                win = false;
+                break;
+            }
+        }
+        if (win) {
+            const combination = [];
+            for (let i = 0; i < n; i++) {
+                combination.push([i, j]);
+            }
+            isOver = true;
+            return [first, combination];
+        }
+    }
+
+    // Проверка главной диагонали
+    let first = field[0][0];
+    if (first !== EMPTY) {
+        let win = true;
+        for (let i = 1; i < n; i++) {
+            if (field[i][i] !== first) {
+                win = false;
+                break;
+            }
+        }
+        if (win) {
+            const combination = [];
+            for (let i = 0; i < n; i++) {
+                combination.push([i, i]);
+            }
+            isOver = true;
+            return [first, combination];
+        }
+    }
+
+    first = field[0][n - 1];
+    if (first !== EMPTY) {
+        let win = true;
+        for (let i = 1; i < n; i++) {
+            if (field[i][n - 1 - i] !== first) {
+                win = false;
+                break;
+            }
+        }
+        if (win) {
+            const combination = [];
+            for (let i = 0; i < n; i++) {
+                combination.push([i, n - 1 - i]);
+            }
+            return [first, combination];
+        }
+    }
+
+    // Если победителя не найдено
     return false;
 }
 
